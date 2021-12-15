fe7fafdb2532175b036ffb261f379bbf6743a1c4
Thales Wendel <sigleane@outlook.com>
2021-12-15 19:30:39 -0300

    page-landing ok. Acess content já iniciado.

--- public/scripts/hover_effect.js
+++ public/scripts/hover_effect.js
@@ -1,8 +1,8 @@
 const materials_card = document.querySelector("#materials");
-const videos_card = document.querySelector("#videos");
+const info_card = document.querySelector("#info");
 
-videos_card.addEventListener("mouseover", inactive_materials_state);
-videos_card.addEventListener("mouseout", active_materials_state)
+info_card.addEventListener("mouseover", inactive_materials_state);
+info_card.addEventListener("mouseout", active_materials_state)
 
 function inactive_materials_state() {
     if(materials_card.classList.contains("active_clip_path")){
